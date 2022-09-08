81c24c35e7bea597e62fad8b083dcc90df009b1b
Evgeny Gridin <jon666gridin@yandex.ru>
2022-09-08 09:21:15 +0200

    test-3

--- models/user.js
+++ models/user.js
@@ -18,7 +18,12 @@ const userSchema = new mongoose.Schema({
   avatar: {
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     type: String,
-    validate: (v) => validator.isURL(v),
+    validate: {
+      validator(link) {
+        return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/.test(link);
+      },
+      message: (props) => `${props.value} ссылка невалидна`,
+    },
   },
   email: {
     type: String,
@@ -37,16 +42,21 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = async (email, password) => {
-  const user = await this.findOne({ email }).select('+password');
-  if (!user) {
-    return Promise.reject(new Error('Неправильные почта или пароль'));
-  }
-  const matched = await bcrypt.compare(password, user.password);
-  if (!matched) {
-    return Promise.reject(new Error('Неправильные почта или пароль'));
-  }
-  return user;
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильная почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильная почта или пароль'));
+          }
+          return user;
+        });
+    });
 };
 
 module.exports = mongoose.model('user', userSchema);
